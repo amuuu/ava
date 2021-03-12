d6f0206ab35625826dea973acbb0c36d981a3e38
amuuu <aamouie01@gmail.com>
2021-03-12 05:18:50 -0500

    add a device map for demo

--- demo/EngineSampleMain.cpp
+++ demo/EngineSampleMain.cpp
@@ -1,46 +1,46 @@
 #include "../src/AvaUserHeaders.h"
 
 #include <iostream>
-
-///////// This is only to use the sleep function to demonstrate the example.
-#ifdef _WIN32
-#include <Windows.h>
-#else
-#include <unistd.h>
-#endif
-//////////////
-
-#define NUM_SECONDS   (3)
-
+#include <map>
 
 int main(void)
 {
 
     Ava* ava = new Ava();
+    
     SimpleSineGenerator* sine = new SimpleSineGenerator();
     SimpleSineGenerator* sine2 = new SimpleSineGenerator();
-    DelayEffect* delay = new DelayEffect();
     
+    std::map<int, SimpleSineGenerator*>* devices = new std::map<int, SimpleSineGenerator*>;
+    devices->insert(std::pair<int, SimpleSineGenerator*>(1, sine));
+    devices->insert(std::pair<int, SimpleSineGenerator*>(2, sine2));
+
     ava->project->GetTrack(0)->SetSoundSource(sine);
-    // ava->project->GetTrack(0)->AppendNewSoundEffect(delay);
     ava->project->AppendTrack();
     ava->project->GetTrack(1)->SetSoundSource(sine2);
 
+    // DelayEffect* delay = new DelayEffect();    
+    // ava->project->GetTrack(0)->AppendNewSoundEffect(delay);
 
-    sine->SetParameter("freq", 510);
+    sine->SetParameter("freq", 440);
     sine->SetParameter("amp", 1);
-
     // ava->project->GetTrack(0)->ChangeTrackState(Deactivated);
 
 
-    // int targetInstrument = 1;
     float newFreq = 440, newAmp = 1;
     float numSeconds = 0.5; 
+
     while (true)
     {
         // std::cin >> targetInstrument;
+
+        std::cout << "new freq: ";
         std::cin >> newFreq;
+        
+        std::cout << "new amp: ";
         std::cin >> newAmp;
+        
+        std::cout << "secs: ";
         std::cin >> numSeconds;
 
         if (newFreq != -1)
@@ -67,7 +67,6 @@ int main(void)
                 }
             }
         }
-
     }
 
     // printf("TEST DONE AND SEE YOU LATER.\n");
