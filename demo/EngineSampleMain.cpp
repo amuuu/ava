9b9f297f63869f228d003083ba79a383e14e353f
amuuu <aamouie01@gmail.com>
2021-03-12 05:23:42 -0500

    add exit input for demo

--- demo/EngineSampleMain.cpp
+++ demo/EngineSampleMain.cpp
@@ -24,52 +24,69 @@ int main(void)
 
     sine->SetParameter("freq", 440);
     sine->SetParameter("amp", 1);
+
+    sine2->SetParameter("freq", 800);
+    sine2->SetParameter("amp", 1);
     // ava->project->GetTrack(0)->ChangeTrackState(Deactivated);
 
 
     float newFreq = 440, newAmp = 1;
     float numSeconds = 0.5; 
+    int deviceNumber = 1;
+    int exit = -1;
 
     while (true)
     {
-        // std::cin >> targetInstrument;
 
-        std::cout << "new freq: ";
-        std::cin >> newFreq;
+        std::cout << "exit? ";
+        std::cin >> exit;
         
-        std::cout << "new amp: ";
-        std::cin >> newAmp;
-        
-        std::cout << "secs: ";
-        std::cin >> numSeconds;
+        if (exit != 1)
+        {
+            // std::cin >> targetInstrument;
+            std::cout << "which device? ";
+            std::cin >> deviceNumber;
 
-        if (newFreq != -1)
-        {
-            sine->SetParameter("freq", newFreq);
-        }
-        
-        if (newAmp != -1)
-        {
-            sine->SetParameter("amp", newAmp);
-        }
-        
-        if (numSeconds != -1)
-        {
-            if (ava->SetState(StartingState))
-            {
-                if (ava->SetState(OutputPlaybackState))
-                {
-                    printf("Play for %d seconds.\n", NUM_SECONDS);
-
-                    Pa_Sleep( numSeconds * 1000);
+            std::cout << "new freq: ";
+            std::cin >> newFreq;
             
-                    ava->SetState(HaltState);
+            std::cout << "new amp: ";
+            std::cin >> newAmp;
+            
+            std::cout << "secs: ";
+            std::cin >> numSeconds;
+
+            if (newFreq != -1)
+            {
+                (*devices)[deviceNumber]->SetParameter("freq", newFreq);
+            }
+            
+            if (newAmp != -1)
+            {
+                (*devices)[deviceNumber]->SetParameter("amp", newAmp);
+            }
+            
+            if (numSeconds != -1)
+            {
+                if (ava->SetState(StartingState))
+                {
+                    if (ava->SetState(OutputPlaybackState))
+                    {
+                        printf("Play for %f seconds.\n", numSeconds);
+
+                        Pa_Sleep( numSeconds * 1000);
+                
+                        ava->SetState(HaltState);
+                    }
                 }
             }
         }
+        else {
+            break;
+        }
     }
 
-    // printf("TEST DONE AND SEE YOU LATER.\n");
+    printf("TEST DONE AND SEE YOU LATER.\n");
     return paNoError;
 }
 
