68a19cacb2ec2f323a721bd2c3a63e8d2ef29d96
amuuu <aamouie01@gmail.com>
2021-03-12 05:08:19 -0500

    implement a basic while true for demo

--- demo/EngineSampleMain.cpp
+++ demo/EngineSampleMain.cpp
@@ -1,5 +1,6 @@
 #include "../src/AvaUserHeaders.h"
 
+#include <iostream>
 
 ///////// This is only to use the sleep function to demonstrate the example.
 #ifdef _WIN32
@@ -17,36 +18,59 @@ int main(void)
 
     Ava* ava = new Ava();
     SimpleSineGenerator* sine = new SimpleSineGenerator();
+    SimpleSineGenerator* sine2 = new SimpleSineGenerator();
     DelayEffect* delay = new DelayEffect();
     
+    ava->project->GetTrack(0)->SetSoundSource(sine);
+    // ava->project->GetTrack(0)->AppendNewSoundEffect(delay);
+    ava->project->AppendTrack();
+    ava->project->GetTrack(1)->SetSoundSource(sine2);
+
 
     sine->SetParameter("freq", 510);
     sine->SetParameter("amp", 1);
-    ava->project->GetTrack(0)->SetSoundSource(sine);
 
-    ava->project->GetTrack(0)->AppendNewSoundEffect(delay);
+    // ava->project->GetTrack(0)->ChangeTrackState(Deactivated);
 
-    // ava.project.GetTrack(0)->ChangeTrackState(Deactivated);
 
-    // SimpleSineGenerator sine2;
-    // sine2.SetParameter("freq", 200);
-    // sine2.SetParameter("amp", 0.5);
-    // ava.project->AppendTrack();
-    // ava.project->GetTrack(1)->SetSoundSource(&sine2);
-
-    
-
-    if (ava->SetState(StartingState))
+    // int targetInstrument = 1;
+    float newFreq = 440, newAmp = 1;
+    float numSeconds = 0.5; 
+    while (true)
     {
-        if (ava->SetState(OutputPlaybackState))
-        {
-            printf("Play for %d seconds.\n", NUM_SECONDS);
+        // std::cin >> targetInstrument;
+        std::cin >> newFreq;
+        std::cin >> newAmp;
+        std::cin >> numSeconds;
 
-            Pa_Sleep( NUM_SECONDS * 1000);
-    
-            ava->SetState(HaltState);
+        if (newFreq != -1)
+        {
+            sine->SetParameter("freq", newFreq);
         }
+        
+        if (newAmp != -1)
+        {
+            sine->SetParameter("amp", newAmp);
+        }
+        
+        if (numSeconds != -1)
+        {
+            if (ava->SetState(StartingState))
+            {
+                if (ava->SetState(OutputPlaybackState))
+                {
+                    printf("Play for %d seconds.\n", NUM_SECONDS);
+
+                    Pa_Sleep( numSeconds * 1000);
+            
+                    ava->SetState(HaltState);
+                }
+            }
+        }
+
     }
-    printf("TEST DONE AND SEE YOU LATER.\n");
+
+    // printf("TEST DONE AND SEE YOU LATER.\n");
     return paNoError;
 }
+
